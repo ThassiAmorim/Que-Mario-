341d8b217dafdb730b9c22fb192f947636ec5980
Thassiana Amorim <thassi.ana.amorim@gmail.com>
2023-09-28 09:30:55 -0300

    jogo pronto v1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
+var s = document.ge
+
+
 
 const jump = () => {
     mario.classList.add('jump')
@@ -12,7 +15,21 @@ const jump = () => {
 const loop = setInterval(() =>{
     
     const pipePosition = pipe.offsetLeft;
-    console.log(pipePosition)
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
+    
+    if((pipePosition <= 120) && (marioPosition < 90) && (pipePosition > 0)){
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+        
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = '/images/game-over.png' 
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop)
+    }
 
 },10)
 
